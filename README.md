5d668387252d764294a526c70d66a174a3a0cc52
teraaret <raiiliiat@yandex.ru>
2018-08-24 18:27:54 +0500

    edit, multiple undo, sorting

--- README.md
+++ README.md
@@ -8,3 +8,11 @@ My todo-list project for vue.js learning
 - Added `undo` function;
 - Added `done` function;
 - Added sorting by status and id;
+
+## 24.08.2018 - v0.4
+
+- Editing posts;
+- `Undo` is a stack with unlimited steps now;
+- Sorting by id now DESC;
+- Controller upgraded;
+- New id assigner repaired;
--- app/controller.php
+++ app/controller.php
@@ -1,11 +1,29 @@
 <?php
 
-    $filename = $_SERVER['DOCUMENT_ROOT'].'/data/data.json';
+    $token = "token123";
 
-    if ( !empty($_POST['content']) )
+    if ( ($_GET['token'] == $token) || ($_POST['token'] == $token) )
     {
-        file_put_contents( $filename, $_POST['content'] );
-        echo "php: file saved";
+        $filename = $_SERVER['DOCUMENT_ROOT'].'/data/data.json';
+
+        if ( $_GET['read'] == 1 )
+        {
+            $data = file_get_contents( $filename );
+            header($_SERVER['SERVER_PROTOCOL']." 200 OK");
+            header('Content-Type: application/json');
+            echo json_encode($data);
+        }
+
+        if ( !empty($_POST['content']) )
+        {
+            file_put_contents( $filename, $_POST['content'] );
+            header($_SERVER['SERVER_PROTOCOL']." 202 Accepted");
+            header('Content-Type: text/plain');
+            echo "php: file saved";
+        }
+    } else {
+        header($_SERVER['SERVER_PROTOCOL']." 418 I'm a teapot");
     }
+
 
 ?>
