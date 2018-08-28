71f5dfef92c4e43fe0e3f78329599f560fcd458d
teraaret <raiiliiat@yandex.ru>
2018-08-28 12:48:49 +0500

    Checking data file exist

--- app/controller.php
+++ app/controller.php
@@ -4,10 +4,18 @@
 
     if ( ($_GET['token'] == $token) || ($_POST['token'] == $token) )
     {
+        $dirname = $_SERVER['DOCUMENT_ROOT'].'/data';
         $filename = $_SERVER['DOCUMENT_ROOT'].'/data/data.json';
 
         if ( $_GET['read'] == 1 )
         {
+            if (!is_dir( $dirname )) {
+                mkdir($_SERVER['DOCUMENT_ROOT']."/data");
+            }
+                
+            if (!file_exists( $filename )) {
+                file_put_contents( $filename, "[]" );
+            }
             $data = file_get_contents( $filename );
             header($_SERVER['SERVER_PROTOCOL']." 200 OK");
             header('Content-Type: application/json');
