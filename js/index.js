e70e94f2e7259623b95ce776d2e61d9f1c51a1cf
Teraaret <raiiliiat@yandex.ru>
2019-11-25 10:59:20 +0500

    Click area fix, v0.6

--- js/index.js
+++ js/index.js
@@ -1,23 +1,18 @@
 var app = new Vue({
     el: '#app',
     data: {
-        search: '', // search input value
-        newTodo: '', // new todo input value
-        todos: [], // todos (loaded from json)
-        edit: undefined, // number of showing edit input
-        undo: [], // undo stack
+        search: '',                     // search input value
+        newTodo: '',                    // new todo input value
+        todos: [],                      // todos (loaded from file)
+        edit: undefined,                // number of showing edit input
+        undo: [],                       // undo stack
         load_endpoint: "/app/load.php", // link to PHP controller
         save_endpoint: "/app/save.php", // link to PHP controller
     },
     methods: {
         addTodo: function () {
-            // check emptiness
-            if (this.newTodo == "") {
-                return false;
-            }
-            if ( this.todos.findIndex(x => x.title == this.newTodo) != -1 ) {
-                return false;
-            }
+            if (this.newTodo == "") return false; // check emptiness
+            if ( this.todos.findIndex(x => x.title == this.newTodo) != -1 ) return false; // check repeat
             this.todos.push({
                 title: this.newTodo,
                 done: false
@@ -26,12 +21,7 @@ var app = new Vue({
             this.saveList();
         },
         editBegin: function (key) {
-            // setting this.edit to number of edited item
-            this.edit = key;
-            // idk why vue do not wants to do it easier...
-//            Vue.nextTick(function () {
-//                $("#input_" + id).focus();
-//            })
+            this.edit = key; // setting this.edit to number of edited item
         },
         editEnd: function (title, same_id) {
             this.edit = undefined;
@@ -65,34 +55,26 @@ var app = new Vue({
         undoDelete: function () {
             // `undo` function pops the last value in the `this.undo` array
             // and pushing it to `this.todos`
-            this.todos.push(this.undo.pop());
-            this.saveList();
+            this.todos.push(this.undo.pop())
+            this.saveList()
         },
         loadList: function () {
-            // there are GET request to PHP controller
-            // it sends READ variable with value 1 and token
-            $.ajaxSetup({
-                cache: false
-            });
-            $.ajaxSetup({
-                async: false
-            });
+            $.ajaxSetup({ cache: false });
+            $.ajaxSetup({ async: false });
             var that = this;
             $.get(this.load_endpoint, function (data) {
                 if (data != "") {
-                    that.todos = JSON.parse(data);
-                    that.sortList();
+                    that.todos = JSON.parse(data)
+                    that.sortList()
                 }
             });
         },
         saveList: function () {
-            // there are POST request to PHP controller
-            // it sends current JSON and token
             var that = this;
             $.post(this.save_endpoint, {
                 content: JSON.stringify(that.todos)
             });
-            that.sortList();
+            that.sortList()
         },
         sortList() {
             // Sorting by `status` in first step
@@ -107,6 +89,6 @@ var app = new Vue({
         },
     },
     mounted: function () {
-        this.loadList();
+        this.loadList()
     }
 });
