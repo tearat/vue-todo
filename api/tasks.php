4d999d2f8217b025c4fa44815e3397de82f3cd30
Teraaret <raiiliiat@yandex.ru>
2020-02-06 18:21:52 +0500

    Tasks api

---
+++ api/tasks.php
@@ -0,0 +1,17 @@
+<?php
+
+$data = file_get_contents('../data/data.json');
+header($_SERVER['SERVER_PROTOCOL']." 200 OK");
+header('Content-Type: application/json');
+$tasks = json_decode($data);
+
+echo "Todo list: \n\n";
+
+foreach ($tasks as $task) {
+    if( $task->done ) {
+        echo "[X] ";
+    } else {
+        echo "[ ] ";
+    }
+    echo $task->title . "\n";
+}
